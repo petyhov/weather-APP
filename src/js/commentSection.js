d6ebf7860c755b2778c397712970e768c04a5c4b
MellYu <mel6273569@gmail.com>
2020-09-11 14:00:26 +0300

    clear derection src

--- src/js/commentSection.js
+++
@@ -1,9 +0,0 @@
-import comments from './commentsArray.js';
-import refs from './refs.js';
-
-
-function getText() {
-  refs.commentTextContent.innerHTML =
-    comments[Math.floor(Math.random() * comments.length)];
-}
-setInterval(getText, 9000);
