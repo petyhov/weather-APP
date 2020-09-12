360c7a57ebb1852846aed51bf9b9884fe45b082c
petyhov <48859592+petyhov@users.noreply.github.com>
2020-09-12 21:50:40 +0300

    5 day section get temperature

--- src/index.js
+++ src/index.js
@@ -3,24 +3,26 @@ import './js/commentSection.js';
 import './js/input.js';
 
 import './js/slick';
-import {defaultCity} from './js/defaultCity.js';
-import {handleInput} from './js/input.js';
-import {getByGeolocation} from './js/geolocationFunc.js';
+import { defaultCity } from './js/defaultCity.js';
+import { handleInput } from './js/input.js';
+import { getByGeolocation } from './js/geolocationFunc.js';
 
-function getGeolocation(){
-    return new Promise((getByGeolocation, defaultCity)=>{
-        navigator.geolocation.getCurrentPosition(getByGeolocation,defaultCity);
-    })
+function getGeolocation() {
+  return new Promise((getByGeolocation, defaultCity) => {
+    navigator.geolocation.getCurrentPosition(getByGeolocation, defaultCity);
+  });
 }
 
-getGeolocation().then(location=>{
+getGeolocation()
+  .then(location => {
     const lat = location.coords.latitude;
     const lon = location.coords.longitude;
-    getByGeolocation({lat, lon});
+    getByGeolocation({ lat, lon });
     document.body.classList.add('.weather__byGeo');
-}).catch(error=>{
-    console.log(error);
-});
+  })
+  .catch(error => {
+    // console.log(error);
+  });
 
 defaultCity();
 handleInput();
