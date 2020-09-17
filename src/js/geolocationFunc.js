e10d559fa2fd4ae6804f9ea44493c9ce3f421063
MellYu <mel6273569@gmail.com>
2020-09-18 01:58:20 +0300

    correct classes

--- src/js/geolocationFunc.js
+++ src/js/geolocationFunc.js
@@ -4,22 +4,23 @@ import { groupByDate } from './groupByDateFunction.js';
 import dateBlock from './createDateBlock.js';
 import { fiveDaysForecast } from './forecastForFiveDays.js';
 
-export function getByGeolocation({ lat, lon }) {
+
+
+
+export function getByGeolocation({lat, lon}) {
   //Блок з датою, світанком та заходом сонця
-  forecastData.getForecastByCurrentPosition({ lat, lon }).then(city => {
-    dateBlock(city);
-    refs.locationWeather.src = `https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`;
-    refs.locationName.textContent = `${city.name}, ${city.sys.country}`;
-    refs.todayTemperature.textContent = Math.round(city.main.temp);
-    refs.todayMinTemperature.textContent = Math.round(city.main.temp_min);
-    refs.todayMaxTemperature.textContent = Math.round(city.main.temp_max);
-  });
-
-  // Блок з прогнозом погоди на 5 днів
-
-  forecastData
-    .getForecastFiveDaysByCurrentPosition({ lat, lon })
-    .then(forecast => {
+    forecastData.getForecastByCurrentPosition({lat, lon}).then(city => {
+      dateBlock(city);
+      refs.locationWeather.src = `https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`;
+      refs.locationName.textContent = `${city.name}, ${city.sys.country}`;
+      refs.todayTemperature.textContent = Math.round(city.main.temp);
+      refs.todayMinTemperature.textContent = Math.round(city.main.temp_min);
+      refs.todayMaxTemperature.textContent = Math.round(city.main.temp_max);
+    });
+  
+    // Блок з прогнозом погоди на 5 днів
+    
+    forecastData.getForecastFiveDaysByCurrentPosition({lat, lon}).then(forecast => {
       const arrData = forecast.list;
       const newArr = groupByDate(arrData);
       newArr.length = 5;
@@ -28,9 +29,10 @@ export function getByGeolocation({ lat, lon }) {
         for (value of el) {
           Object.values(...newArr[0])[1];
         }
-      });
-      // Блок з прогнозом погоди на 5 днів
-
-      fiveDaysForecast('vinnytsia');
     });
-}
+
+// Блок з прогнозом погоди на 5 днів
+
+fiveDaysForecast('Vinnytsia');
+  })};
+    
